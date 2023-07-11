0169aa4e357ebe52279515123458736064c85882
adrianusyoga1p <adrianusyoga26@gmail.com>
2023-07-11 17:02:00 +0700

    oke

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,7 +5,7 @@ import './NavBar.css'
 
 function NavBar() {
     return (
-        <div className='navbar flex flex-wrap items-center gap-[32px]'>
+        <div className='navbar flex flex-wrap items-center gap-[32px] justify-center'>
             <NavLink to="/popular">Popular<HiFire className='ml-2'/></NavLink>
             <NavLink to="/ongoing">Ongoing</NavLink>
             <NavLink to="/newrelease">New Release</NavLink>
