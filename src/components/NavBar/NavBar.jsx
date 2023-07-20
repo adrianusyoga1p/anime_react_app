5d8d300471592eba4a4bc62a741abbe235e6b811
adrianusyoga1p <adrianusyoga26@gmail.com>
2023-07-20 18:37:12 +0700

    mobile-responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import NavLink from './NavLink'
 import { HiFire, HiMenu } from 'react-icons/hi'
 import './NavBar.css'
 
 function NavBar() {
+
+    const [active, setActive] = useState(false);
+
+    let menuRef = useRef();
+
+    useEffect(() => {
+        let handler = (e) => {
+            if(!menuRef.current.contains(e.target)){
+                setActive(false)
+            }
+        };
+        document.addEventListener('mousedown', handler);
+
+        return () => {
+            document.removeEventListener('mousedown', handler)
+        }
+    }, [])
     return (
         <>
             <div className='navbar sm:flex flex-wrap items-center gap-[32px] justify-center hidden'>
@@ -13,15 +30,15 @@ function NavBar() {
                 <NavLink to="/upcoming">Up Coming</NavLink>
             </div>
 
-            <div className='navbar fixed sm:hidden flex items-center justify-center'>
+            <button onClick={()=>setActive(!active)} className='navbar fixed sm:hidden flex items-center justify-center' ref={menuRef}>
                 <HiMenu className='text-white text-[20px]'/>
-                <div className='bg-white w-[200px] h-auto absolute navbar-items'>
+                <div className={`bg-white w-[200px] h-auto absolute navbar-items ${active ? 'active' : ''}`}>
                     <NavLink to="/popular">Popular<HiFire className='ml-2'/></NavLink>
                     <NavLink to="/ongoing">Ongoing</NavLink>
                     <NavLink to="/newrelease">New Release</NavLink>
                     <NavLink to="/upcoming">Up Coming</NavLink>
                 </div>
-            </div>
+            </button>
         </>
     )
 }
